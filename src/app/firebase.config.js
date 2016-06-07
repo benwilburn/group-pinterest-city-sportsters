6b8b95ff4ad68a15bd8428f8a33d682b5ccd723c
Chase Ramsey <ramsey.chase@gmail.com>
2016-06-07 15:00:26 -0500

    Added script for firebase config

---
+++ src/app/firebase.config.js
@@ -0,0 +1,7 @@
+  var config = {
+    apiKey: "AIzaSyACDe9x_uT7QFh4KLd94iqVKG1MuiCizHk",
+    authDomain: "project-907408699296850865.firebaseapp.com",
+    databaseURL: "https://project-907408699296850865.firebaseio.com",
+    storageBucket: "project-907408699296850865.appspot.com",
+  };
+  firebase.initializeApp(config);
