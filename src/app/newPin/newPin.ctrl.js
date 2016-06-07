628a651c5fd8b8e811e7fa95f15d741c73be75cb
Megan Ford <meg.c.ford@gmail.com>
2016-06-07 15:32:47 -0500

    edited buttons

--- src/app/newPin/newPin.ctrl.js
+++ src/app/newPin/newPin.ctrl.js
@@ -1,6 +1,17 @@
 angular.module("sports")
-  .controller("CreateEditCtrl", function() {
-    
+  .controller("CreateEditCtrl", function($location) {
+    const create = this;
+
     console.log("controller working" );
 
-  });
+    create.sendPin = function() {
+      console.log("sendPin Function");
+      $location.path("#/pins");
+    }; //end of sendPin
+
+    create.cancelPin = function() {
+      console.log("cancelPin function");
+      $location.path("#/pins");
+    };//end of cancelPin
+
+  });//end of controller
