39f29e33cd3953dd18cee2be619bc5908d192349
Chase Ramsey <ramsey.chase@gmail.com>
2016-06-08 09:50:46 -0500

    Deleted extra console.logs

--- src/app/auth/authFactory.js
+++ src/app/auth/authFactory.js
@@ -4,19 +4,15 @@ angular.module('sports')
     let currentUser = null;
 
     firebase.auth().onAuthStateChanged((user) => {
-      console.log("firebase auth state change", user);
       if (user) {
         var users = null;
         $http.get('http://project-907408699296850865.firebaseio.com/auth.json')
           .then((res) => {
             users = res.data;
-            console.log("Users", users);
             return users;
           })
           .then((users) => {
             for (var key in users) {
-              console.log("users: ", users);
-              console.log("key id: ", users[key].uid);
               if (users[key].uid === user.uid) {
                 currentUser = users[key];
                 console.log("currentUser: ", currentUser);
