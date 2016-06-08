97911479273f3d16511155f7001f6695abe8942e
Megan Ford <meg.c.ford@gmail.com>
2016-06-08 10:23:09 -0500

    form completed and creates object in controller

--- src/app/newPin/newPin.ctrl.js
+++ src/app/newPin/newPin.ctrl.js
@@ -2,11 +2,27 @@ angular.module("sports")
   .controller("CreateEditCtrl", function($location) {
     const create = this;
 
+    setTimeout(function() {
+      $("select").material_select();
+    }, 100);
+
+
+    create.boards = [
+      "sports", "animals", "fruit"
+    ];
+
+    // create.pin = {
+    //   name: "", 
+    //   url: "", 
+    //   desc: "", 
+    //   board: ""
+    // };
+
     console.log("controller working" );
 
     create.sendPin = function(pinToAdd) {
-      console.log("sendPin Function");
-      $location.path("#/pins");
+      console.log("sendPin Function", create.pin);
+      // $location.path("#/pins");
     }; //end of sendPin
 
     create.cancelPin = function() {
