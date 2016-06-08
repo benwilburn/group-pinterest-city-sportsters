c85fe32e44369589e2f6fc7876021f8289a4839c
Chase Ramsey <ramsey.chase@gmail.com>
2016-06-08 14:37:40 -0500

    Added navbar and logout functionality
    Navbar is added outside the 'sports' app. It has a click listener than runs an anonymous callback in main.js that runs the firebase logout. I also removed the route for LogoutCtrl in the route config, and I deleted the file logout.ctrl.js

--- src/app/main.js
+++ src/app/main.js
@@ -1,1 +1,8 @@
 angular.module('sports', ['ngRoute']);
+
+// Navbar logic
+
+var btnLogout = document.getElementById("logout");
+btnLogout.addEventListener("click", function() {
+  firebase.auth().signOut();
+});
